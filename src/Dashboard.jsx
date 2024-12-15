c5113282bcd27c5980bca98ef6d6e6b906160a4c
John_Paul Cambiado <jpcambiado@gbox.ncf.edu.ph>
2024-12-15 20:24:55 +0800

    6th

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -219,7 +219,7 @@ function Dashboard() {
             <Nav.Link as={Link} to="/home" className="navbar-link-services">🏠 Home</Nav.Link>
             <Nav.Link as={Link} to="/contact" className="navbar-link-services">📞 Contact</Nav.Link>
 
-            <NavDropdown title={user ? `User: ${user.username}` : 'Dropdown'} id="basic-nav-dropdown" align="end">
+            <NavDropdown className="dropdown-basic-nav-dashboard" title={user ? `User: ${user.username}` : 'Dropdown'} id="basic-nav-dropdown" align="end">
               <NavDropdown.Item as={Link} to="/Profile">Profile</NavDropdown.Item>
               <NavDropdown.Item href="#">Settings</NavDropdown.Item>
               <NavDropdown.Item href="#" onClick={handleLogout}>Logout</NavDropdown.Item>
